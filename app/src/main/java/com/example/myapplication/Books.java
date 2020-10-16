87f773f8400055d1ff2f08c71f8f51bb7b736bb2
AliHejazi1382 <mrhjz08@gmail.com>
2020-10-16 14:59:55 +0330

    Books class edited

--- app/src/main/java/com/example/myapplication/Books.java
+++ app/src/main/java/com/example/myapplication/Books.java
@@ -1,4 +1,72 @@
 package com.example.myapplication;
 
 public class Books {
+    private String aCategory;
+    private String title;
+    private String aLang;
+    private String author;
+    private int year;
+    private String photo;
+    private double price;
+
+    public String getaCategory() {
+        return aCategory;
+    }
+
+    public void setaCategory(String aCategory) {
+        this.aCategory = aCategory;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getaLang() {
+        return aLang;
+    }
+
+    public void setaLang(String aLang) {
+        this.aLang = aLang;
+    }
+
+    public String getAuthor() {
+        return author;
+    }
+
+    public void setAuthor(String author) {
+        this.author = author;
+    }
+
+    public int getYear() {
+        return year;
+    }
+
+    public void setYear(int year) {
+        this.year = year;
+    }
+
+    public String getPhoto() {
+        return photo;
+    }
+
+    public void setPhoto(String photo) {
+        this.photo = photo;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+
+    public void setPrice(double price) {
+        this.price = price;
+    }
+
+    @Override
+    public String toString() {
+        return title;
+    }
 }
