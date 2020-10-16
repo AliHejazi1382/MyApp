7e41fe4ed56bee0b5e1c8f40874afd2cb1693826
AliHejazi1382 <mrhjz08@gmail.com>
2020-10-16 15:06:14 +0330

    XmlGJDOMParser class edited

--- app/src/main/java/com/example/myapplication/XmlGJDOMParser.java
+++ app/src/main/java/com/example/myapplication/XmlGJDOMParser.java
@@ -1,4 +1,40 @@
 package com.example.myapplication;
 
-public class XmlGJDOMParser {
+import org.jdom2.Document;
+import org.jdom2.Element;
+import org.jdom2.JDOMException;
+import org.jdom2.input.SAXBuilder;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.ArrayList;
+import java.util.List;
+
+public abstract class XmlGJDOMParser<T> {
+
+    public abstract InputStream getInput();
+
+    public abstract String getChildrenName();
+
+    public abstract T getObject();
+
+    public List<T> parseXml() {
+        List<T> t = new ArrayList<>();
+        SAXBuilder builder = new SAXBuilder();
+        try {
+            Document document = builder.build(getInput());
+            Element rootElement = document.getRootElement();
+            List<Element> elements = rootElement.getChildren(getChildrenName());
+            for (Element e : elements){
+                T item = getObject();
+                t.add(item);
+            }
+            getInput().close();
+        } catch (JDOMException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return t;
+    }
 }
