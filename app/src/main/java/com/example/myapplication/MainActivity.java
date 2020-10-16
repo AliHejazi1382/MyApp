80787b88991c546549036a5e22799e8bfef1b35b
AliHejazi1382 <mrhjz08@gmail.com>
2020-10-16 15:42:40 +0330

    the third test was successful

--- app/src/main/java/com/example/myapplication/MainActivity.java
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -35,7 +35,8 @@ public class MainActivity extends AppCompatActivity {
         btnJdom.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-
+                books = new XmlJdomParser(MainActivity.this).parseXml();
+                refreshDisplay();
             }
         });
 
