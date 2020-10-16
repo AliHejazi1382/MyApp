7904315ebcca9a90ef2dd561de6708b9dc98807b
AliHejazi1382 <mrhjz08@gmail.com>
2020-10-16 14:38:28 +0330

    the widgets declared and inited

--- app/src/main/java/com/example/myapplication/MainActivity.java
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -3,12 +3,21 @@ package com.example.myapplication;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.ListView;
 
 public class MainActivity extends AppCompatActivity {
 
+    private Button btnGJdom, btnJdom, btnGManual, btnManual;
+    private ListView listView;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        btnGJdom = findViewById(R.id.btnGJdom);
+        btnJdom = findViewById(R.id.btnJdom);
+        btnGManual = findViewById(R.id.btnGManual);
+        btnManual = findViewById(R.id.btnManual);
+        listView = findViewById(R.id.listView);
     }
 }
