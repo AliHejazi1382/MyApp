cab23932a9c271c92e4f84bb328bcf59d95c1b72
AliHejazi1382 <mrhjz08@gmail.com>
2020-10-16 15:14:43 +0330

    BookJdomParser class edited

--- app/src/main/java/com/example/myapplication/BookJdomParser.java
+++ app/src/main/java/com/example/myapplication/BookJdomParser.java
@@ -1,4 +1,38 @@
 package com.example.myapplication;
 
-public class BookJdomParser {
+import android.content.Context;
+
+import org.jdom2.Element;
+
+import java.io.InputStream;
+
+public class BookJdomParser extends XmlGJDOMParser<Books>{
+    private Context context;
+    public BookJdomParser(Context context) {
+        this.context = context;
+    }
+
+    @Override
+    public InputStream getInput() {
+        return context.getResources().openRawResource(R.raw.bookstore);
+    }
+
+    @Override
+    public String getChildrenName() {
+        return "book";
+    }
+
+    @Override
+    public Books getObject(Element e) {
+        Books book = new Books();
+        book.setaCategory(e.getAttributeValue("category"));
+        book.setTitle(e.getChildText("title"));
+        book.setaLang(e.getAttributeValue("lang"));
+        book.setAuthor(e.getChildText("author"));
+        book.setYear(Integer.valueOf(e.getChildText("year")));
+        book.setPhoto(e.getChildText("photo"));
+        book.setPrice(Double.valueOf(e.getChildText("price")));
+        return book;
+    }
+
 }
